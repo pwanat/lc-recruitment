f2523bf10e594d9874f71ab6c61b0421613b82be
Paweł Wanat <pawel.wanat111@gmail.com>
2025-05-08 14:59:51 +0200

    fix: add OR search

--- src/components/canned-responses/helpers.ts
+++ src/components/canned-responses/helpers.ts
@@ -1,7 +1,7 @@
-import { privacyFilterCannedResponses } from "../../hooks/helpers/privacy-filter-canned-responses";
-import { searchInString } from "../../hooks/helpers/search";
-import { CannedResponse } from "../../types/canned-responses";
-import { CannedResponseFilterType } from "../../types/filter-type";
+import { privacyFilterCannedResponses } from '../../hooks/helpers/privacy-filter-canned-responses';
+import { searchInString } from '../../hooks/helpers/search';
+import { CannedResponse } from '../../types/canned-responses';
+import { CannedResponseFilterType } from '../../types/filter-type';
 
 export function getExtractedText(text: string, maxTextLength = 150): string {
   return text.length > maxTextLength ? `${text.substring(0, maxTextLength - 3)}...` : text;
@@ -11,15 +11,25 @@ export function filterAvailableTags(tags: string[], availableTagNames: Set<strin
   return availableTagNames.size > 0 ? tags.filter((tag) => availableTagNames.has(tag)) : tags;
 }
 
-export const filteredAndSortedResponses = (responses: CannedResponse[], filter: CannedResponseFilterType): CannedResponse[] =>
+export const filteredAndSortedResponses = (
+  responses: CannedResponse[],
+  filter: CannedResponseFilterType,
+): CannedResponse[] =>
   privacyFilterCannedResponses(responses, filter).sort(
     (first, second) => second.modificationTimestamp - first.modificationTimestamp,
   );
 
 export const searchInItem = (item: CannedResponse, search: string): boolean => {
   if (!search) return true;
+
   const { text, tags, createdBy } = item;
-  return (
-    searchInString(text, search) || searchInString(tags.join(' '), search) || searchInString(createdBy || '', search)
-  );
+  const searchWords = search.trim().toLowerCase().split(/\s+/);
+
+  const isMatched = searchWords.some((word) => {
+    const isInText = searchInString(text, word);
+    const isInTags = tags.some((tag) => searchInString(tag, word));
+    const isInCreatedBy = createdBy ? searchInString(createdBy, word) : false;
+    return isInText || isInTags || isInCreatedBy;
+  });
+  return isMatched;
 };
